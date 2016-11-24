90b7707c16e6a07e3219f32cfe607a2cfddfa7a0
Matthew <matthew@jobadder.com>
2016-11-24 14:05:31 +1100

    Add DisableAutoCorrectEffect

---
+++ Source/Organon.XForms.Effects/Organon.XForms.Effects/DisableAutoCorrectEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Organon.XForms.Effects
+{
+	/// <summary>
+	/// When attached to a Xamarin.Forms.Entry control, disables auto-suggestions and auto-correction for entered text.
+	/// </summary>
+	public class DisableAutoCorrectEffect : RoutingEffect
+	{
+		public DisableAutoCorrectEffect() : base("Organon.Effects.DisableAutoCorrectEffect")
+		{
+		}
+	}
+}
