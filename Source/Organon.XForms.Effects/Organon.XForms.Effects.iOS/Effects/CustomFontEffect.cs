463c7ea50866700c448d6743bd1c50ec840ff852
Gerald Versluis <gerald@verslu.is>
2016-11-30 11:26:33 +0100

    Added CustomFontEffect for iOS and Android
    Including screenshots for the label effects and updated the Readme.md

---
+++ Source/Organon.XForms.Effects/Organon.XForms.Effects.iOS/Effects/CustomFontEffect.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Organon.XForms.Effects.iOS;
+using UIKit;
+using System.Linq;
+
+[assembly: ExportEffect (typeof (CustomFontEffect), nameof (CustomFontEffect))]
+namespace Organon.XForms.Effects.iOS
+{
+    public class CustomFontEffect : PlatformEffect
+    {
+        protected override void OnAttached ()
+        {
+            var control = Control as UILabel;
+
+            if (control != null) {
+                var effect = (XForms.Effects.CustomFontEffect)Element.Effects.FirstOrDefault (item => item is XForms.Effects.CustomFontEffect);
+                if (effect != null && !string.IsNullOrWhiteSpace (effect.FontPath)) {
+                    control.Font = UIFont.FromName (effect.FontFamilyName, control.Font.PointSize);
+                }
+            }
+        }
+
+        protected override void OnDetached ()
+        {
+        }
+    }
+}
